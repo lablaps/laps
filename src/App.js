4e2d570fc82b50d0d87f06669b30eff77a57735c
luis <40541878+Luluzao0@users.noreply.github.com>
2025-01-12 19:22:15 -0300

    alters

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import AppRoutes from './routes/AppRoutes';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 import './assets/styles/css/bootstrap.min.css'
 
 const App = () => {
@@ -9,8 +8,6 @@ const App = () => {
     <>
       <Navbar />
       <AppRoutes />
-      <Footer/>
-
     </>
   );
 };
