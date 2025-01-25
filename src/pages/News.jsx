e07bbd86055ba89228e26a78fa0fd1e7b6bd2a29
Patrick <pm221100@gmail.com>
2025-01-25 12:07:12 -0300

    News Update
    adding images e making modifications on news page

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -4,8 +4,8 @@ import "../assets/styles/css/News.css";
 function News() {
   return (
     <>
-    {/* Header Start */}
-    <div className="container-fluid bg-primary py-5 mb-5 page-header">
+      {/* Header Start */}
+      <div className="container-fluid bg-primary py-5 mb-5 page-header">
         <div className="container py-5">
           <div className="row justify-content-center">
             <div className="col-lg-10 text-center">
@@ -32,36 +32,55 @@ function News() {
         </div>
       </div>
       {/* Header End */}
-    <div className="news-container">
-      <h1 className="news-title">Acompanhe as últimas notícias do LAPS</h1>
-      <p className="news-intro">
-        Fique por dentro dos eventos, publicações e novidades relacionados ao
-        Laboratório de Aquisição e Processamento de Sinais (LAPS). Aqui você
-        encontrará atualizações constantes sobre nossas pesquisas, projetos e
-        colaborações.
-      </p>
 
-      {/* Exemplo de "card" simples para uma notícia */}
-      <div className="news-card">
-        <h2 className="news-card-title">Título da Notícia</h2>
-        <p className="news-card-resumo">
-          Resumo da notícia para informar aos visitantes. Destaque pontos mais
-          importantes e ofereça uma prévia do que podem esperar ao ler mais.
-        </p>
-        <button className="news-button">Leia Mais</button>
+      {/* News Start */}
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-lg-4 mb-4">
+            <div className="card border-0 shadow-sm rounded">
+              <div className="card-img-top news-card-img news-card-img-1"></div>
+              <div className="card-body">
+                <h5 className="card-title">Assessoria de Comunicação Institucional</h5>
+                <p className="card-text">
+                  Conheça o Laboratório de Aquisição e Processamento de Sinais da Uema
+                </p>
+                <a href="https://www.uema.br/2024/07/conheca-o-laboratorio-de-aquisicao-e-processamento-de-sinais-da-uema/" className="btn btn-primary">
+                  Leia Mais
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 mb-4">
+            <div className="card border-0 shadow-sm rounded">
+              <div className="card-img-top news-card-img news-card-img-2"></div>
+              <div className="card-body">
+                <h5 className="card-title">Instagram UEMA</h5>
+                <p className="card-text">
+                  Quando inovação e ciência se unem, o Maranhão dá um salto no futuro! 🚀
+                </p>
+                <a href="https://www.instagram.com/p/DCR9WYixeQa/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" className="btn btn-primary">
+                  Leia Mais
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 mb-4">
+            <div className="card border-0 shadow-sm rounded">
+              <div className="card-img-top news-card-img news-card-img-3"></div>
+              <div className="card-body">
+                <h5 className="card-title">Redação Imirante</h5>
+                <p className="card-text">
+                  Laboratório de Aquisição e Processamento de Sinais da Uema estimula o desenvolvimento científico e tecnológico do estado
+                </p>
+                <a href="https://imaranhense.com/noticia/23500/laboratorio-de-aquisicao-e-processamento-de-sinais-da-uema-estimula-o-desenvolvimento-cientifico-e-tecnologico-do-estado" className="btn btn-primary">
+                  Leia Mais
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div className="news-card">
-        <h2 className="news-card-title">Título da Notícia</h2>
-        <p className="news-card-resumo">
-          Resumo da notícia para informar aos visitantes. Destaque pontos mais
-          importantes e ofereça uma prévia do que podem esperar ao ler mais.
-        </p>
-        <button className="news-button">Leia Mais</button>
-      </div>
-
-      {/* Você pode repetir o bloco .news-card com outras notícias */}
-    </div>
+      {/* News End */}
     </>
   );
 }
